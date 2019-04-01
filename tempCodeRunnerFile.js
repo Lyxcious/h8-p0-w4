e715f607d1298e5c60bb6bdccd0c53c0c2922bfe
lyxcious <felix_laurensius@yahoo.com>
2019-04-01 18:45:00 +0700

    update

--- tempCodeRunnerFile.js
+++
@@ -1,4 +0,0 @@
-  console.log(digitPerkalianMinimum(90)); // 3
-  console.log(digitPerkalianMinimum(20)); // 2
-  console.log(digitPerkalianMinimum(179)); // 4
-  console.log(digitPerkalianMinimum(1)); // 2
